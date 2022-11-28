a13df28072563cac361f97a5197c6ed1f9bd19f6
VeeV01 <ezigbovo@gmail.com>
2022-11-27 18:10:27 -0800

    airbnb

---
+++ models/city.py
@@ -0,0 +1,10 @@
+import uuid
+from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ City class """
+    state_id = ""
+    name = ""
